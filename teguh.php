390d2fa173de7a010eefe775f17e723d3e2a91d8
Muhammad-Teguh <47422846+Muhammad-Teguh@users.noreply.github.com>
2019-02-07 22:55:53 +0800

    Rename index.php to teguh.php

---
+++ teguh.php
@@ -0,0 +1,7 @@
+<table width = "337">
+
+<td width ="298" hight="300" align="center">
+<p><strong> : TUGAS 2 CLOUD COMPUTING </strong></p>
+<p><strong> : Oleh Muhammad Teguh </strong></p>
+
+</table>
